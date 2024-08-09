5480c09b6ae2fbd397b0e017b3051231c65a1eb3
sona2711 <sonamkrtchyan2711@gmail.com>
2024-08-09 20:48:45 +0400

    added time info wrapper

--- clock.js
+++ clock.js
@@ -1,23 +1,29 @@
 const hourHand = document.querySelector(".hour-hand");
 const secHand = document.querySelector(".sec-hand");
 const minHand = document.querySelector(".min-hand");
+const hour = document.querySelector(".hour");
+const minute = document.querySelector(".minute");
+const second = document.querySelector(".second");
 
 function setDate() {
     const now = new Date()
-    //secounds hand part
-    const secounds = now.getSeconds();
-    const secDegree = ((secounds / 60) * 360 )+ 90;
+    //seconds hand part
+    const seconds = now.getSeconds();
+    const secDegree = ((seconds / 60) * 360 )+ 90;
     secHand.style.transform = `rotate(${secDegree}deg)`;
+    second.innerHTML = seconds
 
     //minutes hand part
     const minutes = now.getMinutes();
     const minDegree = ((minutes / 60 )* 360)+ 90;
     minHand.style.transform = `rotate(${minDegree}deg)`;
+    minute.innerHTML = minutes
 
-    //hour hand part
-    const hour = now.getHours()
-    const hourDegree = ((hour / 60 )* 360)+ 90;
+    //hours hand part
+    const hours = now.getHours()
+    const hourDegree = ((hours / 60 )* 360)+ 90;
     hourHand.style.transform = `rotate(${hourDegree}deg)`;
+    hour.innerHTML = hours
 
     
 
